c572565836385084847d42cbe6cf0b049f2d58e2
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-05-06 15:19:58 +0100

    add avatar component library and configs

---
+++ src/avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-avatar',
+  template: `<h1>Angualar 2 Avatar component</h1>`
+})
+export class AvatarComponent {
+
+  constructor() {
+  }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AvatarComponent } from './avatar.component';
+import { AvatarService } from './avatar.service';
+
+export * from './avatar.component';
+export * from './avatar.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    AvatarComponent
+  ],
+  exports: [
+    AvatarComponent
+  ]
+})
+export class AvatarModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AvatarModule,
+      providers: [AvatarService]
+    };
+  }
+}
