8daf507b2e71bad12158f1a20de2badc59cb70b2
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-05-25 10:20:34 +0100

    remove avatar service and reorganizing imports

---
+++ src/sources/utils.ts
@@ -0,0 +1,57 @@
+
+export type AvatarSource =
+    'facebook' |
+    'google' |
+    'twitter' |
+    'skype' |
+    'gravatar' |
+    'custom' |
+    'initials';
+
+/**
+ * list of Supported avatar sources
+ */
+export const sources = [
+  "FACEBOOK",
+  "GOOGLE",
+  "TWITTER",
+  "SKYPE",
+  "GRAVATAR",
+  "CUSTOM",
+  "INITIALS",
+  "VALUE"];
+
+/**
+ * list of default colors
+ */
+export const defaultColors = [
+    "#1abc9c",
+    "#3498db",
+    "#f1c40f",
+    "#8e44ad",
+    "#e74c3c",
+    "#d35400",
+    "#2c3e50",
+    "#7f8c8d"
+];
+
+
+/**
+ * Get a random color 
+ * 
+ * @export
+ * @returns {string} 
+ */
+export function getRandomColor(colors = defaultColors): string {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+/**
+ * Get source priority 
+ * Facebook has the highest priority, Value has the lowest 
+ * @param source 
+ * @param avatarSources 
+ */
+export function getSourcePriority(source: string, avatarSources = sources){
+    return sources.indexOf(source.toUpperCase());
+}
