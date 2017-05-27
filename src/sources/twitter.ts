8043fa41824725d9808a9ca8b6bd8e42f4353a13
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-05-27 17:00:08 +0100

    add image size

--- src/sources/twitter.ts
+++ src/sources/twitter.ts
@@ -14,6 +14,21 @@ export class Twitter implements Source {
     constructor(public sourceId: string) {
     }
     getAvatar(size:number): string {
-        return `https://twitter.com/${this.sourceId}/profile_image?size=${size}`;
+        let twitterImgSize = this._getImageSize(size);
+        return `https://twitter.com/${this.sourceId}/profile_image?size=${twitterImgSize}`;
     }
+     
+    _getImageSize(size:number){
+        if (size <= 24)
+            return 'mini';
+
+        if (size <= 48)
+            return 'normal';
+
+        if (size <= 73)
+            return 'bigger';
+
+        return 'original';
+    }
+
 }
