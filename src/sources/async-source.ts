c4bedb8e2eb13f184537a7fdf23b6e5296cef927
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-08-14 10:52:06 +0100

    #11: rename async source file

---
+++ src/sources/async-source.ts
@@ -0,0 +1,16 @@
+import { Source } from "./source";
+
+/**
+ * Contract of all async sources.
+ * Every async source must implement the processResponse method that extracts the avatar url from the data
+ * @interface AsyncSource
+ */
+export abstract class AsyncSource implements Source {
+    readonly sourceType: string;
+
+    constructor(public sourceId: string) { }
+
+    abstract getAvatar(size?: number): string;
+    abstract processResponse(data: any, size?: number): string;
+
+}
