e349e15fc4dbe43e5fbb1a8a978390cc175485bc
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-08-16 19:39:05 +0100

    #10 : add injection token for the avatarConfig object

---
+++ src/avatar-config.token.ts
@@ -0,0 +1,8 @@
+import { AvatarConfig } from './avatar-config';
+import { InjectionToken } from '@angular/core';
+/**
+ * Token used to inject the AvatarConfig object
+ */
+export let AVATAR_CONFIG = new InjectionToken<AvatarConfig>('avatar.config');
+
+    
