ffb25d292bd064479f4dc9a0d3d67587dfcb1cc7
Haithem Mosbahi <elmosbahihaithem@gmail.com>
2017-08-17 18:44:26 +0100

    release version 2.6.1

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { UserService } from "./user.service";
-import { AvatarModule } from "ngx-avatar";
+import { AvatarModule,AvatarConfig } from "ngx-avatar";
 import { HttpModule } from "@angular/http";
 
 @NgModule({
@@ -12,8 +11,10 @@ import { HttpModule } from "@angular/http";
   imports: [
     BrowserModule,
     HttpModule,
-    // import AvatarModule in your app
-    AvatarModule
+    // import AvatarModule in your app with your own configuration
+    AvatarModule.forRoot(new AvatarConfig(['#e74c3c','#2c3e50','#95a5a6','#f39c12','#1abc9c']))
+    // or import the AvatarModule with its default options
+    // AvatarModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
