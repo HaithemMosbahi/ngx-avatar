44ca77c2a15662df3f69df7f15018cb6d5e510e2
HaithemMosbahi <elmosbahihaithem@gmail.com>
2018-10-03 10:07:22 +0200

    setup TestBed for app component

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+
+import { AppComponent } from './app.component';
+import { UserService } from './user.service';
+
+const userServiceStub = {
+    fetchInformations: jasmine.createSpy('userService.fetchInformations'),
+    getUserFacebook: jasmine.createSpy('userService.getUserFacebook')
+}
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      providers:[
+        {provide: UserService, use: userServiceStub}  
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});
