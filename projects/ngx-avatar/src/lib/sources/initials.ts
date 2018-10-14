aa041272b4ed88f3bbab073ef77f6c0f644960b6
Luiz Machado <machado@odahcam.com>
2018-10-14 19:38:48 -0300

    trim empty names plus docs

--- projects/ngx-avatar/src/lib/sources/initials.ts
+++ projects/ngx-avatar/src/lib/sources/initials.ts
@@ -2,23 +2,34 @@ import { Source } from "./source";
 import { AvatarSource } from "./avatar-source.enum";
 
 /**
- *  Initials source impelementation.
- *  return the initals of the given value
+ * Initials source impelementation.
+ * return the initals of the given value
  */
 export class Initials implements Source {
+  
   readonly sourceType: AvatarSource = AvatarSource.INITIALS;
 
-  constructor(public sourceId: string) {}
+  constructor(
+    public sourceId: string
+  ) { }
 
   public getAvatar(initialsSize: number): string {
     return this.getInitials(this.sourceId, initialsSize);
   }
 
+  /**
+   * Returns the initial letters of a name in a string.
+   */
   private getInitials(name: string, size: number): string {
+    
+    name = name ? name.trim() : null;
+    
     if (!name) {
       return '';
     }
-    const initials = name.trim().split(" ");
+    
+    const initials = name.split(" ");
+    
     if (size && size < initials.length) {
       return this.constructInitials(initials.slice(0, size));
     } else {
@@ -26,6 +37,9 @@ export class Initials implements Source {
     }
   }
 
+  /**
+   * Iterates a person's name string to get the initials of each word in uppercase.
+   */
   private constructInitials(elements: string[]): string {
     if (!elements || !elements.length) {
       return '';
