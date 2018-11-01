4dd775eb963926102e7179961bf27b29bbe499f3
Luiz Machado <machado@odahcam.com>
2018-10-31 21:04:09 -0300

    doc comments

--- projects/ngx-avatar/src/lib/avatar-config.ts
+++ projects/ngx-avatar/src/lib/avatar-config.ts
@@ -1,10 +1,17 @@
 import { AvatarSource } from "./sources/avatar-source.enum";
 
 /**
- * Represents avatar configuration object
+ * Represents avatar configuration object.
  */
 export interface AvatarConfig {
+  
+  /**
+   * The avatars colors.
+   */
   colors?: string[];
 
+  /**
+   * The order in which the avatar sources will be used.
+   */
   sourcePriorityOrder?: AvatarSource[];
 }
